cb4f3938213f1ec2b564f6601ea406959533a7f3
Timo <timo02@me.com>
2018-10-24 09:49:04 +0200

    Added more CSS
    Added image

--- scripts/script.js
+++
@@ -1,7 +0,0 @@
-let button = document.getElementById('rotateBtn');
-let body = document.querySelector('body');
-
-button.addEventListener('dblclick', () => {
-	body.classList.add('rotate')
-})
-if $(())
